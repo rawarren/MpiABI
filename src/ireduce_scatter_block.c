78cd5ac6b3cedaab51ddb4968aa3715f441fc874
Richard Warren <Richard.Warren@hdfgroup.org>
2020-01-29 15:41:16 -0500

    Added some missing Asynch collective functions

---
+++ src/ireduce_scatter_block.c
@@ -0,0 +1,48 @@
+
+/*
+ * This text was generated automatically
+ * It provides the ISC implementation of
+ * MPI_Ireduce_scatter
+ */
+
+#include <_mpi.h>
+
+int
+MPI_Ireduce_scatter_block(void *sendbuf, void *recvbuf, int recvcount, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm, MPI_Request *request)
+{
+  static void *address=0;
+  int mpi_return;
+
+  if (!address) {
+    if ((address = dlsym(MPI_libhandle,"MPI_Ireduce_scatter_block")) == NULL) {
+      printf("%s %s %s",SYM_MISSING_PREFIX,"MPI_Ireduce_scatter_block",SYM_MISSING_TRAILER);
+      return -1;
+    }
+  }
+  *request = new_index(active_requests);
+  
+  if (active_datatypes->use_ptrs) { api_use_ptrs *local_a0 = active_datatypes->api_declared;
+    api_use_ptrs *local_a1=active_ops->api_declared;
+    api_use_ptrs *local_a2= active_comms->api_declared;
+    if (active_requests->use_ptrs) { api_use_ptrs *local_a3=active_requests->api_declared;
+      int (*VendorMPI_Ireduce_scatter_block)(void *sendbuf,void *recvbuf,int recvcount,void *,void *,void *, void **) = address;
+      mpi_return = (*VendorMPI_Ireduce_scatter_block)(INPLACE(sendbuf),INPLACE(recvbuf),recvcount,local_a0[datatype].mpi_const,local_a1[op].mpi_const,local_a2[comm].mpi_const,&local_a3[*request].mpi_const);
+    }
+    else { api_use_ints *local_a3=active_requests->api_declared;
+      int (*VendorMPI_Ireduce_scatter_block)(void *sendbuf,void *recvbuf,int recvcount,void *,void *,void *, int *) = address;
+      mpi_return = (*VendorMPI_Ireduce_scatter_block)(INPLACE(sendbuf),INPLACE(recvbuf),recvcount,local_a0[datatype].mpi_const,local_a1[op].mpi_const,local_a2[comm].mpi_const,&local_a3[*request].mpi_const);
+    }
+  } else { api_use_ints *local_a0 = active_datatypes->api_declared;
+    api_use_ints *local_a1=active_ops->api_declared;
+    api_use_ints *local_a2= active_comms->api_declared;
+    if (active_requests->use_ptrs) { api_use_ptrs *local_a3=active_requests->api_declared;
+      int (*VendorMPI_Ireduce_scatter_block)(void *sendbuf,void *recvbuf,int recvcount,int,int,int,void **) = address;
+      mpi_return = (*VendorMPI_Ireduce_scatter_block)(INPLACE(sendbuf),INPLACE(recvbuf),recvcount,local_a0[datatype].mpi_const,local_a1[op].mpi_const,local_a2[comm].mpi_const,&local_a3[*request].mpi_const);
+    }
+    else { api_use_ints *local_a3=active_requests->api_declared;
+      int (*VendorMPI_Ireduce_scatter_block)(void *sendbuf,void *recvbuf,int recvcount,int,int,int,int *) = address;
+      mpi_return = (*VendorMPI_Ireduce_scatter_block)(INPLACE(sendbuf),INPLACE(recvbuf),recvcount,local_a0[datatype].mpi_const,local_a1[op].mpi_const,local_a2[comm].mpi_const,&local_a3[*request].mpi_const);
+    }
+  }
+  return mpi_return;
+}
