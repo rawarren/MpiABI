5bc1b61a61e5355d6d7ab9407d36e293c6263a92
Richard Warren <Richard.Warren@hdfgroup.org>
2020-01-31 16:51:36 -0500

    Added some missing functionality for the File_set_errhandler

--- src/file_set_errhandler.c
+++ src/file_set_errhandler.c
@@ -20,13 +20,26 @@ MPI_File_set_errhandler ( MPI_File file, MPI_Errhandler errhandler)
     }
   }
   if (active_files->use_ptrs) { api_use_ptrs *local_a0=active_files->api_declared;
-    api_use_ptrs *local_a1=active_errhandlers->api_declared;
-    int (*VendorMPI_File_set_errhandler)(void *,void *) = address;
-    mpi_return = (*VendorMPI_File_set_errhandler)(local_a0[file].mpi_const,local_a1[errhandler].mpi_const);
+      if (active_errhandlers->use_ptrs) {
+	  api_use_ptrs *local_a1=active_errhandlers->api_declared;
+	  int (*VendorMPI_File_set_errhandler)(void *,void *) = address;
+	  mpi_return = (*VendorMPI_File_set_errhandler)(local_a0[file].mpi_const,local_a1[errhandler].mpi_const);
+      }
+      else { api_use_ints *local_a1=active_errhandlers->api_declared;
+	  int (*VendorMPI_File_set_errhandler)(void *, int) = address;
+	  mpi_return = (*VendorMPI_File_set_errhandler)(local_a0[file].mpi_const,local_a1[errhandler].mpi_const);
+      }
   } else { api_use_ints *local_a0=active_files->api_declared;
-    api_use_ints *local_a1=active_errhandlers->api_declared;
-    int (*VendorMPI_File_set_errhandler)(int,int) = address;
-    mpi_return = (*VendorMPI_File_set_errhandler)(local_a0[file].mpi_const,local_a1[errhandler].mpi_const);
+      if (active_errhandlers->use_ptrs) {
+	  api_use_ptrs *local_a1=active_errhandlers->api_declared;
+	  int (*VendorMPI_File_set_errhandler)(int,void *) = address;
+	  mpi_return = (*VendorMPI_File_set_errhandler)(local_a0[file].mpi_const,local_a1[errhandler].mpi_const);
+      }
+      else {
+	  api_use_ints *local_a1=active_errhandlers->api_declared;
+	  int (*VendorMPI_File_set_errhandler)(int,int) = address;
+	  mpi_return = (*VendorMPI_File_set_errhandler)(local_a0[file].mpi_const,local_a1[errhandler].mpi_const);
+      }
   }
   return mpi_return;
 }
